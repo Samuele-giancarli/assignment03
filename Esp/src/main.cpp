75309b19989dec977e6da241ffcdb529ebbe8a78
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2024-04-17 16:05:21 +0200

    Esp is connecting to wifi, but not to server

--- Esp/src/main.cpp
+++ Esp/src/main.cpp
@@ -11,6 +11,12 @@
 
 String ssid = "OPPO A9 2020";
 String pass = "vv6miwa8";
+String brokerUser = "ginevra.bartolini@studio.unibo.it";
+String brokerPass = "4Rc8mak_01";
+String broker = "mqtt.dioty.co";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void wifiSetup() {
   delay(10000);
@@ -29,15 +35,32 @@ void wifiSetup() {
   delay(10000);
 }
 
+void reconnect() {
+  while(!client.connected()) {
+    Serial.print("\nConnecting");
+    if(client.connect("koikoikoi", brokerUser.c_str(), brokerPass.c_str())){
+      Serial.print("\nConnected to");
+      Serial.println(broker);
+    } else {
+      Serial.print("Trying connecting again");
+      delay(10000);
+    }
+  }
+}
+
 void setup() {
   // Start the serial communication with the ESP device
   Serial.begin(460800);
   wifiSetup();
+  client.setServer(broker.c_str(), 1883);
   Serial.println("aaaaaaa, World!");
 }
 
 void loop() {
   // Print a test message
+  if(!client.connected()){
+    reconnect();
+  }
    Serial.print("\nScemo");
    delay(1000);
   // Wait for a second
